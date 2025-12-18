4fdcfa4b5c2022c36c7ce7d7174025487e84aee1
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2025-12-18 19:19:24 +0500

    create button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
       <h1>Hello</h1>
       <p>Welcome to the App component!</p>
       <p>Bye</p>
+      <button>click</button>
+      <button>onclick</button>
     </div>
   );
 };
