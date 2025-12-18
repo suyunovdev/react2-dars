3eb4cf2ef89e5dddb697c8ff11cd3bbefa690523
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2025-12-18 19:01:57 +0500

    create file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Welcome to the App component!</p>
+      <p>Bye</p>
+    </div>
+  );
+};
+export default App;
